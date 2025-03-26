7f40e5424bd69e026349b20d520d7d9821f7341d
palash-wp-dev <raps.palash.840@gmail.com>
2025-03-26 13:51:34 +0600

    Added script and css file for this project

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const currentDate = new Date();
+
+let date = currentDate.getDate();
+let month = currentDate.getMonth();
+let year = currentDate.getFullYear();
+
+console.log(date + ' ' + month + ' ' + year  );
