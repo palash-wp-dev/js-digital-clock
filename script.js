17ec46aad1d5e5bc8eb22adb06bb753419bd683c
palash-wp-dev <raps.palash.840@gmail.com>
2025-04-05 12:26:37 +0600

    Updated the code

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 'use strict';
 
-const currentDate = new Date();
+function updateTime() {
+    const currentDate = new Date();
 
-let date = currentDate.getDate();
-let month = currentDate.getMonth();
-let year = currentDate.getFullYear();
+    // get the dates
+    let date = currentDate.getDate();
+    let month = currentDate.getMonth();
+    let year = currentDate.getFullYear();
 
-console.log(date + ' ' + month + ' ' + year  );
+    // get the hours
+    let seconds = currentDate.getSeconds();
+    let minutes = currentDate.getMinutes();
+    let hours = currentDate.getHours();
+    // let version = currentDate.get();
+
+    let fullDate = date + '/' + month + '/' + year;
+    let fullTime = hours +':'+ minutes + ' ' + seconds;
+
+    document.getElementById('date').innerText = fullDate;
+    document.getElementById('clock').innerHTML = fullTime;
+}
+
+setInterval(updateTime,1000);
