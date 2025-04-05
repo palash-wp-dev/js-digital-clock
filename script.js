79766840b4ca5ff1595f80c4a242766efc1f8cac
palash-wp-dev <raps.palash.840@gmail.com>
2025-04-05 19:17:54 +0600

    solved the am/pm issue

--- script.js
+++ script.js
@@ -3,22 +3,15 @@
 function updateTime() {
     const currentDate = new Date();
 
-    // get the dates
-    let date = currentDate.getDate();
-    let month = currentDate.getMonth();
-    let year = currentDate.getFullYear();
+    
+    let fullTime = currentDate.toLocaleTimeString();
 
-    // get the hours
-    let seconds = currentDate.getSeconds();
-    let minutes = currentDate.getMinutes();
-    let hours = currentDate.getHours();
-    // let version = currentDate.get();
-
-    let fullDate = date + '/' + month + '/' + year;
-    let fullTime = hours +':'+ minutes + ' ' + seconds;
+    let fullDate = currentDate.toLocaleDateString();
 
     document.getElementById('date').innerText = fullDate;
     document.getElementById('clock').innerHTML = fullTime;
+
+    // console.log(version);
 }
 
 setInterval(updateTime,1000);
